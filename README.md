8d989873224d0ec081d915fff7c4a5aa47d116af
Saampave <Saampave_96@hotmail.com>
2023-11-30 18:48:49 +0000

    Update code with latest changes

---
+++ README.md
@@ -0,0 +1,6 @@
+# Hangman
+## Table of Contents
+1. Favourite fruit
+2. Random selection from the list
+3. Choose a single character
+4. Check the chosen character is a letter
--- milestone_2.py
+++ milestone_2.py
@@ -1,3 +1,18 @@
+import random
+# list of my five favourite fruits
 favourite_fruit = ['Mango','Lychee','Grapes','Melon','Peach']
+#assign the list to a variable called word_list
 word_list = favourite_fruit
 print(word_list)
+
+# randomly generates words from list
+word = random.choice(word_list)
+print(word)
+
+# asking user to choose a single letter
+guess = input('Enter a single letter: ')
+# check guess is alphabetical
+if len(guess) == 1 and guess.isalpha():
+   print('Good guess!')
+else:
+    print('Oops! That is not a valid option.')
